108c3d5c35fd0cd1f6972e968ad9340d9292b2bb
yacob <nestor.jac@gmail.com>
2014-02-19 23:30:49 -0500

    implement snapping for cube object

--- Assets/Scripts/MakeGrid.cs
+++ Assets/Scripts/MakeGrid.cs
@@ -6,6 +6,9 @@ public class MakeGrid : MonoBehaviour {
 	public Transform squarePrefab;
 	public int width;
 	public int length;
+	public float heightPos = 0;
+	public float xPos = 0;
+	public float zPos = 0;
 	public bool checkered = true;
 	
 
@@ -20,9 +23,9 @@ public class MakeGrid : MonoBehaviour {
 		for (int x = 0; x < width; x++) {
 			for (int z = 0; z < length; z++) {
 				Transform newSquare;
-				newSquare = (Transform)Instantiate(squarePrefab, new Vector3 (x, 0, z), Quaternion.identity);
+				newSquare = (Transform)Instantiate(squarePrefab, new Vector3 (x + xPos, heightPos, z + zPos), Quaternion.identity);
 				newSquare.parent = transform;	//puts grid as parent of gameSquare
-				newSquare.name = "(" + x + ", " + z + ")";	//names square
+				newSquare.name = "(" + x + ", " + heightPos + ", " + z + ")";	//names square
 			}
 		}
 	}
@@ -38,6 +41,37 @@ public class MakeGrid : MonoBehaviour {
 				square.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
 			}
 		}
+	}
+	
+	public Vector3 snapHeight(Vector3 temp){
+		temp.y = heightPos + 1;
+		return temp;
+	}
+	public Vector3 snap(Vector3 temp){
+		temp = snapHeight(temp);
+		if(temp.x < xPos){
+			temp.x = xPos;
+		}
+		else if(temp.x > xPos + width){
+			temp.x = xPos + width - 1;
+		}
+		if(temp.z < zPos){
+			temp.z = zPos;
+		}
+		else if(temp.z > zPos + length){
+			temp.z = zPos + length - 1;
+		}
+		float offset = temp.x - xPos;
+		offset = Mathf.Round(offset);
+		temp.x = offset + xPos;
+		Debug.Log("offset after rounding for x " + offset);
+		
+		offset = temp.z - zPos;
+		offset = Mathf.Round(offset);
+		temp.z = offset + zPos;
+		Debug.Log("offset after rounding for z " + offset);
+		return temp;
+		
 	}
 
 }
---
+++ Assets/Scripts/cubeScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeScript : MakeGrid {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	
+	void Update () {
+		bool up = Input.GetKey(KeyCode.UpArrow);
+		bool down = Input.GetKey(KeyCode.DownArrow);
+		bool right = Input.GetKey(KeyCode.RightArrow);
+		bool left = Input.GetKey(KeyCode.LeftArrow);
+		if(up){
+			Vector3 temp = transform.position;
+			temp.z += 0.1f;
+			transform.position = temp;
+		}
+		if(down){
+			Vector3 temp = transform.position;
+			temp.z -= 0.1f;
+			transform.position = temp;
+		}
+		if(right){
+			Vector3 temp = transform.position;
+			temp.x += 0.1f;
+			transform.position = temp;
+		}
+		if(left){
+			Vector3 temp = transform.position;
+			temp.x -= 0.1f;
+			transform.position = temp;
+		}
+		if(!up && !down && !left && !right){
+			Vector3 pos = transform.position;
+			pos = snap(pos);
+			transform.position = pos;
+		}
+	}
+}
